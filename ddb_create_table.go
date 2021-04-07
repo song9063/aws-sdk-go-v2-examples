7ba1fadec5d4a56cf2cc229b2ae42a27e60792eb
Song Junwoo <busang.song@gmail.com>
2021-04-08 00:59:01 +0900

    Create ddb_create_table.go

---
+++ ddb_create_table.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+func main() {
+	const tableName = "new-table"
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := dynamodb.NewFromConfig(cfg)
+	params := &dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("PK"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+			{
+				AttributeName: aws.String("SK"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("PK"),
+				KeyType:       types.KeyTypeHash,
+			},
+			{
+				AttributeName: aws.String("SK"),
+				KeyType:       types.KeyTypeRange,
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	}
+	output, err := client.CreateTable(context.TODO(), params)
+	// /*for debug*/
+	if output != nil {
+		jsonOutput, _ := json.MarshalIndent(output, "", "\t")
+		fmt.Println(string(jsonOutput))
+	}
+
+	if err != nil {
+		fmt.Println("Error!")
+		log.Fatal(err)
+	}
+}
